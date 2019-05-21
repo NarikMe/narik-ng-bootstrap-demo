c180979091d5ccc8d07dd5c7c7f086ed5c07d0a1
ali badakhshan <abadakhshan@gmail.com>
2019-05-21 10:23:40 +0430

    change favicon

--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -6,16 +6,6 @@ import { NarikUiNgBootstrapModule } from "narik-ui-ng-bootstrap";
 import { NarikSwimlaneDataTableModule } from "narik-ui-swimlane";
 import { COMPONENTS } from "./index";
 
-export const PICK_FORMATS = {
-  parse: { dateInput: { month: "short", year: "numeric", day: "numeric" } },
-  display: {
-    dateInput: "input",
-    monthYearLabel: { year: "numeric", month: "short" },
-    dateA11yLabel: { year: "numeric", month: "long", day: "numeric" },
-    monthYearA11yLabel: { year: "numeric", month: "long" }
-  }
-};
-
 @NgModule({
   imports: [
     CommonModule,
