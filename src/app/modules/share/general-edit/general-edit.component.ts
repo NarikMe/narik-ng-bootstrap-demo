66f82230d6d4126e1c27b5a02e9665b565d6214c
ali <abadakhshan@gmail.com>
2020-08-20 11:53:30 +0430

    Upgrade to Narik 4

--- src/app/modules/share/general-edit/general-edit.component.ts
+++ src/app/modules/share/general-edit/general-edit.component.ts
@@ -1,12 +1,11 @@
-import { NarikBaseTemplate, DynamicForm } from "@narik/core";
+import { DynamicForm } from "@narik/core";
 import { NarikUiEditForm } from "@narik/ui-ng-bootstrap";
 
 import { Component, Injector } from "@angular/core";
 
 @DynamicForm("GeneralEditComponent")
-@NarikBaseTemplate("NarikEditUi")
 @Component({
-  templateUrl: "general-edit.component.html"
+  templateUrl: "general-edit.component.html",
 })
 export class GeneralEditComponent extends NarikUiEditForm<any> {
   constructor(injector: Injector) {
